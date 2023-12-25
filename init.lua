05c8ece9c05aedcfd24855d65905bf63cba752f6
Tadeu Ascoli <tascoli@gmail.com>
2023-12-24 23:23:41 -0300

    Refatored init.lua and lazy_conf.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
+-- Autor: Tadeu Ascoli
+-- Country: Brazil
+-- e-mail: tascoli@gmail.com
 
+-- General Modules
 require("user.core.options")        -- General neovim options(numberlines, syntax on, etc..)
 require("user.core.keymaps")        -- Keymaps nvim build-in and plugins
---require("user.core.autocommand")  -- TODO: Need review this module
+require("user.core.autocommand")  -- TODO: Need review this module
 
+-- Lazy Module
 require("user.lazy_conf")           -- Lazy configuration and plugins
