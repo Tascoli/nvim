e392e6f16a81dc33679278d3ea26ad7bdc890822
Tadeu Ascoli <tascoli@gmail.com>
2023-12-26 20:28:55 -0300

    Updade Colorscheme

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -4,20 +4,43 @@
 
 return {
 
+-- NIGHTFLY GUICOLORS
     {
     "bluz71/vim-nightfly-guicolors",
     priority = 1000, -- make sure to load this before all the other start plugins
---    config = function()
-      -- load the colorscheme here
---      vim.cmd([[colorscheme nightfly]])
---    end,
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000 ,
     config = function()
       -- load the colorscheme here
-        vim.cmd([[colorscheme gruvbox]])
+      vim.cmd([[colorscheme nightfly]])
     end,
-  },
+    },
+
+-- GRUVBOX
+
+    {
+    "ellisonleao/gruvbox.nvim",
+        priority = 1000 ,
+--      config = function()
+--          -- load the colorscheme here
+--          vim.cmd([[colorscheme gruvbox]])
+--      end,
+    },
+
+-- NIGHTFOX
+
+    { "EdenEast/nightfox.nvim",
+        priority = 1000,
+--        require("nightfox").setup({
+--            opition = {
+--                stykes = {
+--                    comments = "italic",
+--                    keywords = "bold",
+--                    types = "italic, bold",
+--                }
+--            }
+--        })
+--      config = function()
+--          -- load the colorscheme here
+--              vim.cmd([[colorscheme nightfox]])
+--      end,
+    },
 }
