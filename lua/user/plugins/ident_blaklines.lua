293dd46ceacda70b7a93825c289d9044d823c239
Tadeu Ascoli <tascoli@gmail.com>
2024-05-27 11:43:17 -0300

    Add Indent-blankline.nvim

---
+++ lua/user/plugins/ident_blaklines.lua
@@ -0,0 +1,27 @@
+-- Autor: Tadeu Ascoli
+-- Country: Brazil
+-- e-mail: tascoli@gmail.com
+
+return {
+
+    {   "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        opts = {},
+        config = function()
+        local highlight = {
+            "CursorColumn",
+            "Whitespace",
+        }
+        require("ibl").setup {
+            indent = { highlight = highlight, char = "" },
+            whitespace = {
+                highlight = highlight,
+                remove_blankline_trail = false,
+            },
+            scope = { enabled = false },
+            }
+        end,
+    },
+
+}
+
