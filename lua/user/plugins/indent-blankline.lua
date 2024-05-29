e13fd4354ad5fd99e28249d579df3bdc0e29b42f
Tadeu Ascoli <tascoli@gmail.com>
2024-05-29 18:36:35 -0300

    Add Indent Blankline plugin

--- lua/user/plugins/indent-blankline.lua
+++ lua/user/plugins/indent-blankline.lua
@@ -13,13 +13,13 @@ return {
             "Whitespace",
         }
         require("ibl").setup {
-            indent = { highlight = highlight, char = "" },
+            indent = { highlight = highlight, char = ""},
             whitespace = {
                 highlight = highlight,
                 remove_blankline_trail = false,
             },
             scope = { enabled = false },
-            }
+        }
         end,
     },
 
