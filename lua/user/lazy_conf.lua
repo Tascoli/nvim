21fc81c5f89602c8667b77785c6360115ad124c6
Tadeu Ascoli <tascoli@gmail.com>
2024-09-28 21:49:04 -0300

    Add modules in lazy_conf.lua

--- lua/user/lazy_conf.lua
+++ lua/user/lazy_conf.lua
@@ -10,8 +10,8 @@ if not vim.loop.fs_stat(lazypath) then
   -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
-
+-- vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.opt.rtp:prepend(lazypath)
 --------------------------------------------------------------------------------
 
 
@@ -29,4 +29,25 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 -- This way the plugin manager acess the directory 'plugins' and recive the plugin like a function put into  differents files.
 --
 
-require ("lazy").setup("user.plugins")
+-- require ("lazy").setup("user.plugins")
+require("lazy").setup({ {import = "user.plugins" }},
+    {
+    checker = {
+        enabled = true,
+        notify = false,
+        },
+    change_detection = {
+        notify = false,
+        },
+    })
+
+
+local modules = {"core.autocommand", "core.options", "core.keymaps"}
+
+for _, mod in ipairs(modules) do
+    local ok, err = pcall(require, mod)
+    -- config.custom may be empty. It's a optional module
+    -- if not ok and not mod == "config/custom" then
+    --     error(("Error loading %s...\n\n%s"):format(mod, err))
+    -- end
+end
