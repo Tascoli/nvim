98f6ddf040b16c80053ad53fb6eb5cfa7bc7fdae
Tadeu Ascoli <tascoli@gmail.com>
2024-10-06 21:04:29 -0300

    Add Catppucin colorscheme

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -65,23 +65,89 @@ return {
 
     {
     "EdenEast/nightfox.nvim",
-        priority = 1000,
-        setup = function()
-            require("nightfox").setup({
-            opition = {
-                stykes = {
-                    comments = "italic",
-                    keywords = "bold",
-                    types = "italic, bold",
+        -- priority = 1000,
+        -- setup = function()
+        --     require("nightfox").setup({
+        --     opition = {
+        --         stykes = {
+        --             comments = "italic",
+        --             keywords = "bold",
+        --             types = "italic, bold",
+        --         },
+        --     },
+        -- })
+        -- end,
+        -- config = function()
+        --     -- load the colorscheme here
+        --     vim.cmd([[colorscheme nightfox]])
+        -- end,
+    },
+
+
+-- CATPPUCCIN
+
+  {
+    "catppuccin/nvim",
+      priority = 1000,
+      name = "catppuccin",
+      setup = function()
+         require("catppuccin").setup({
+            flavour = "mocha", -- latte, frappe, macchiato, mocha
+            background = { -- :h background
+                light = "latte",
+                dark = "mocha",
+            },
+            transparent_background = true, -- disables setting the background color.
+            show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+            term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+            dim_inactive = {
+                enabled = false, -- dims the background color of inactive window
+                shade = "dark",
+                percentage = 0.15, -- percentage of the shade to apply to the inactive window
+                },
+            no_italic = false, -- Force no italic
+            no_bold = false, -- Force no bold
+            no_underline = false, -- Force no underline
+            styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+                comments = { "italic" }, -- Change the style of comments
+                conditionals = { "italic" },
+                loops = {},
+                functions = {"bold"},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+                operators = {},
+                -- miscs = {}, -- Uncomment to turn off hard-coded styles
+                },
+            color_overrides = {},
+            custom_highlights = {},
+            default_integrations = true,
+            integrations = {
+                cmp = true,
+                gitsigns = true,
+                nvimtree = true,
+                treesitter = true,
+                notify = false,
+                indent_blankline = {
+                  enabled = true,
+                  scope_color = "", -- catppuccin color (eg. `lavender`) Default: text
+                  colored_indent_levels = true,
+                },
+                mini = {
+                    enabled = true,
+                    indentscope_color = "",
                 },
             },
-        })
-        end,
-        config = function()
-            -- load the colorscheme here
-            vim.cmd([[colorscheme nightfox]])
-        end,
-    },
+      })
+      end,
+      config = function()
+        vim.cmd("colorscheme catppuccin") --catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+      end,
+  },
 
 -- DOOM_ONE
 
